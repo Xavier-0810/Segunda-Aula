dc1185659b55111e6ed5471f1ef342808876b6ba
victor xavier <vx92412@gmail.com>
2025-01-12 20:25:25 -0300

    Segunda aula

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+     
+        // Operadores Arietmeticos;
+
+        System.out.println (6+6);
+        System.out.println (5-5);
+        System.out.println(7*4);
+        System.out.println (8/4);
+        
+        // Resto da Operação
+        System.out.println(7%3);
+
+    
+    }
+}
+    
